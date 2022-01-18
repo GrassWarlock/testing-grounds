7919622f21ab6c59c48b339bc58eae808bddf999
GrassWarlock <unmercifulpie@gmail.com>
2022-01-18 23:54:10 +0300

    testing on httpbin first

--- to_stackoverflow.py
+++ to_stackoverflow.py
@@ -1,5 +1,19 @@
 import requests
 
+base_url = "https://httpbin/"
+title = ""
+body = ""
+article_type = ""
+tags = []
+key = ""
+access_token = ""
+
+payload = {"title" : title, "body" : body, "article_type" : article_type, "tags" : tags, "key" : key, "access_token" : access_token}
+
+r = requests.get(base_url + "post?", params=payload)
+
+print(r.text)
+'''
 base_url = "https://api.stackexchange.com/"
 title = ""
 body = ""
@@ -13,3 +27,4 @@ payload = {"title" : title, "body" : body, "article_type" : article_type, "tags"
 r = requests.get(base_url + "2.3/articles/add?", params=payload)
 
 print(r.text)
+'''
