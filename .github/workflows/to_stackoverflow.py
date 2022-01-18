b3d8f03865de827ffe986d5f719f637def3e99bb
GrassWarlock <unmercifulpie@gmail.com>
2022-01-18 23:41:46 +0300

    added github actions files

---
+++ .github/workflows/to_stackoverflow.py
@@ -0,0 +1,15 @@
+import requests
+
+base_url = "https://api.stackexchange.com/"
+title = ""
+body = ""
+article_type = ""
+tags = []
+key = ""
+access_token = ""
+
+payload = {"title" : title, "body" : body, "article_type" : article_type, "tags" : tags, "key" : key, "access_token" : access_token}
+
+r = requests.get(base_url + "2.3/articles/add?", params=payload)
+
+print(r.text)
