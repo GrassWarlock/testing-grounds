0c4bd5283bfad08b10f40b8666929edc124e9cfd
GrassWarlock <unmercifulpie@gmail.com>
2022-01-19 21:27:13 +0300

    major improvements

--- to_stackoverflow.py
+++ to_stackoverflow.py
@@ -2,24 +2,32 @@ import sys
 import requests
 
 base_url = "https://api.stackexchange.com/"
-changed_files = sys.argv
-article_files = []
-for i in changed_files:
-    if i.endswith('.md'):
-        article_files.append(i)
-articles = {}
-for i in article_files:
-    f = open(i, 'r')
-    articles['d'] = {"title" : "", "body" : "", "article_type" : ""}
-title = ""
-body = ""
-article_type = ""
-tags = []
-key = ""
-access_token = ""
+target_file = sys.argv[1]
 
-payload = {"title" : title, "body" : body, "article_type" : article_type, "tags" : tags, "key" : key, "access_token" : access_token}
+if target_file.endswith('.md'):
+    operation = sys.argv[2]
+    lines = open(target_file, 'r').readlines()
 
-r = requests.get(base_url + "2.3/articles?pagesize=2&order=desc&sort=activity&site=stackoverflow")
 
-print(r.text, r.url)
+    for line in lines:
+        if line.startswith("#"):
+            title = line.replace("#", "", 1)
+            lines.remove(line)
+            break
+    body = lines.join("\n")
+    article_type = "knowledge-article"
+    tags = [x for x in target_file.split("/") if len(x) > 1]
+    key = sys.argv[3]
+    access_token = sys.argv[4]
+
+    payload = {"title" : title, "body" : body, "article_type" : article_type, "tags" : tags, "key" : key, "access_token" : access_token}
+    print(payload)
+    if operation == "add":
+        print("ad")
+    if operation == "modify":
+        print("modify")
+    if operation == "remove":
+        print("remove")
+    r = requests.get(base_url + "2.3/articles?pagesize=2&order=desc&sort=activity&site=stackoverflow")
+
+    print(r.text, r.url)
